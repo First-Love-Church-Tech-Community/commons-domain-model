946ca33f227f0540f432a4320cf7161f5f8dbed4
David Inusah <haqinusah7@gmail.com>
2026-09-19 19:19:03 +0000

    docs: render Mermaid diagrams

---
+++ docs/javascripts/mermaid.js
@@ -0,0 +1,7 @@
+document$.subscribe(function () {
+  mermaid.initialize({
+    startOnLoad: false,
+    theme: document.body.getAttribute("data-md-color-scheme") === "slate" ? "dark" : "default"
+  });
+  mermaid.run({ querySelector: ".mermaid" });
+});
